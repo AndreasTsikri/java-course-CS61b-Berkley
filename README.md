80602a67361e3e5e3149f07fb232000c7a4686b7
Sesligas <noEmail>
2025-12-02 18:02:04 +0200

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Solution to HW and projects on CS61B Berkley (online) course on JAVA!
 
 > HW3 -> Lists, Sets, Maps 
+
+
+"skeleton-fa" folder is cloned from Berkley's github folder on "https://github.com/Berkeley-CS61B/skeleton-fa25"
---
+++ skeleton-fa25/hw02/src/StarTriangle5.java
@@ -0,0 +1,41 @@
+public class StarTriangle5 {
+   /**
+     * Prints a right-aligned triangle of stars ('*') with 5 lines.
+     * The first row contains 1 star, the second 2 stars, and so on. 
+     */
+   private static final String sstar= "*****";
+   private static final String ssp= "     ";
+
+   public static String getFromStr(String str, int i){
+   
+	   if(i < 0 || i > 5)
+		   return null;
+
+	   switch(i){
+		   case(0):return "";
+		   case(5):return str;
+		   default : return str.substring(5 - i); 
+	   }
+
+   }
+   public static String getStars(int i){
+ 	return StarTriangle5.getFromStr(StarTriangle5.sstar,i);	   
+   }
+   public static String getSpaces(int i){
+ 	return StarTriangle5.getFromStr(StarTriangle5.ssp,i);	   
+   }
+   public static void starTriangle5() {
+
+	int[] a = {1,2,3,4,5};
+	for(var i : a){
+	   String ss = StarTriangle5.getStars(i);
+	   String sp = StarTriangle5.getSpaces(5 - i);
+	   System.out.format("%s%s \n",sp,ss);
+	}
+   }
+   
+   public static void main(String[] args) {
+      IO.println("Printing 5 stars : ");
+      starTriangle5();
+   }
+}
